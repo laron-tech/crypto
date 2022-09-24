f3bfe08761e6e7a0ed1fe100041f35e9f186793a
curiousdev04 <curiousdev04@gmail.com>
2022-09-24 18:56:28 +0700

    rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -17,5 +17,38 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+//! # Laron Crypto
+//! Laron Crypto is a library for signing and verifying messages using Secp256k1 elliptic curve
+//! algorithm.
+//!
+//! This library is a wrapper for [k256](https://docs.rs/crate/k256/0.11.5) crate.
+//! This library also provides a simple way to generate a private key and a public key. and also a
+//! valid ethereum address.
+//!
+//! ## Example
+//! ```rust
+//! use laron_crypto::{keys::*, common::*};
+//!
+//! // Generate a new private key and a public key
+//! let sk = PrivateKey::new();
+//! let pk = sk.public();
+//!
+//! // Generate a valid ethereum address from the public key
+//! let addr = Address::from_public(&pk);
+//!
+//! // Sign a message
+//! let msg = b"Hello World!";
+//! let sig = sk.sign(msg);
+//! assert!(pk.verify(msg, &sig));
+//!
+//! // or use other way
+//! let sig = signer::sign(msg, &sk);
+//! assert!(signer::verify(msg, &sig, &pk));
+//!
+//! // Recover the public key from the signature
+//! let recovered_pk = signer::recover(msg, &sig).unwrap();
+//! assert_eq!(pk, recovered_pk);
+//! ```
+
 pub mod common;
-pub mod crypto;
+pub mod keys;
